949a18bdc916044b782d76753ead7d1bdd163064
Zeesuper <Zeesuper@DESKTOP-8SQ9T81>
2019-03-20 16:09:51 +0800

    11

---
+++ src/main/java/org/Zeesuper/weinxin_1/controller/MessageReceiverController.java
@@ -0,0 +1,24 @@
+package org.Zeesuper.weinxin_1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/lzc/message/receiver")
+public class MessageReceiverController {
+	
+	public String echo(
+			@RequestParam("signature") String signature,
+			@RequestParam("timestamp") String timestamp,
+			@RequestParam("nonce") String nonce,
+			@RequestParam("echostr") String echostr
+			
+			) {
+		
+		return echostr;
+	}
+	
+
+}
